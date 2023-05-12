4f85a724a3da105c1227b10b74a89e89a81886e0
JooJ <jorge@jooj.dev>
2023-05-12 02:11:04 -0300

    Novas funções
    app/models/Endereco.php
    - Adicionada chamada para o trait de SoftDelete.
    - Definidos os atributos de tipo fillable.
    - Criada uma função para possibilitar a leitura dos fillable por outras funções.
    
    app/Controllers/EnderecoController.php
    - Salvo a função para busca por CEP, as funções estão finalizadas e comentadas quando necessário.

--- app/Http/Controllers/EnderecoController.php
+++ app/Http/Controllers/EnderecoController.php
@@ -2,28 +2,97 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Endereco;
 use Illuminate\Http\Request;
 
 class EnderecoController extends Controller
 {
     
     /**
-     * @param array $dados: 
+     * 
+     * Seria mais interessante realizar essa inserção dentro de uma função em "Endereco.php"
+     * caso tratamentos ou operações adicionais fossem efetuados, como o caso descrito abaixo.
+     *
+     * Honestamente, o correto seria verificar se um registro com mesmo CEP já existe. Caso sim,
+     * verificar se o nome da rua é igual.
+     * Caso seja, segue sem interromper.
+     * Caso não seja, é possível que exista algo errado em algum dado e um tratamento adicional
+     * seria necessário.
+     *
+     * Também acredito que teria sido mais inteligente eu ter separado o CEP, rua, bairro e número
+     * em uma tabela separada tal como acontece com o banco de dados dos Correios.
+     *
+     * @param array $request: input() deve conter ao menos os parâmetros obrigatórios em Endereco->fillable.
      */
     public function create(Request $request) {
-        // TODO
+        try {
+            $endereco = Endereco::create([
+                'rua'         => $request->input('rua'),
+                'complemento' => $request->input('complemento'),
+                'bairro'      => $request->input('bairro'),
+                'numero'      => $request->input('numero'),
+                'cep'         => $request->input('cep'),
+                'cidade_id'   => $request->input('cidade_id'),
+                'estado_id'   => $request->input('estado_id')
+            ]);
+
+            return response($endereco);
+        } catch (\Exception $e) {
+            // Por motivos de segurança e melhor UX, seria ideal tratar as mensagens de erro dessas exceções
+            // de uma forma não técnica em uma situação de uso real.
+            return response($e->getMessage(), 418);
+        }
     }
     
+    /**
+     * @param int $request->id: Esse parâmetro deve ter sido informado na URL.
+     */
     public function read(Request $request) {
-        // TODO
+        $endereco = Endereco::find($request->id);
+        if(!empty($endereco)) {
+            return response($endereco);
+        }
+        return response('Endereço não encontrado.', 404);
     }
     
+    /**
+     * @param int $request->id: Esse parâmetro deve ter sido informado na URL.
+     * @param array $request: input() deve conter os parâmetros cujos valores devem ser alterados.
+     */
     public function update(Request $request) {
-        // TODO
+        $endereco = Endereco::find($request->id);
+        if(empty($endereco)) {
+            response('Endereço não encontrado.', 404);
+        }
+
+        // Verifica automaticamente se existem atributos equivalentes entre request e objeto.
+        foreach($request->input() as $atr => $valor) {
+            if(!in_array($atr, $endereco->getFillable())) continue;
+            $endereco->$atr = $valor;
+        }
+
+        try {
+            $endereco->save();
+            return response($endereco);
+        } catch (\Exception $e) {
+            // Por motivos de segurança e melhor UX, seria ideal tratar as mensagens de erro dessas exceções
+            // de uma forma não técnica em uma situação de uso real.
+            return response($e->getMessage(), 418);
+        }
     }
     
+    /**
+     * Realiza o soft delete de um endereço.
+     * @param int $request->id: Esse parâmetro deve ter sido informado na URL.
+     */
     public function delete(Request $request) {
-        // TODO
+        $endereco = Endereco::find($request->id);
+        if(empty($endereco)) {
+            return response('Endereço não encontrado.', 404);
+        }
+        
+        $endereco->delete();
+        return response('Endereco excluído.', 200);
     }
     
     public function buscarViaCep(Request $request) {
--- app/Models/Endereco.php
+++ app/Models/Endereco.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Endereco extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    
+    /**
+     * Atributos da tabela.
+     */
+    protected $fillable = [
+        'rua',
+        // Complemento é opcional.
+        'complemento',
+        'bairro',
+        'numero',
+        'cep',
+        'cidade_id',
+        'estado_id'
+    ];
+
+    /**
+     * Permite a leitura dos campos de atribuição em massa por outros componentes.
+     */
+    public function getFillable() {
+        return $this->fillable;
+    }
 }
